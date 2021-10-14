dc24e4762c3ae818beda01abc2295e625efff7c5
Paweł Olkiewicz <olkpaw@gmail.com>
2021-10-14 20:43:00 +0200

    Day 15

--- day15/aoc-15.py
+++ day15/aoc-15.py
@@ -1,20 +1,47 @@
-import math
-
-
 def load(name):
     file = open(name, "r")
-    lines = file.readlines()
+    content = file.read()
     file.close()
-    return [line[:-1] for line in lines]
+    return [int(line[:]) for line in content.split(',')]
+
+
+def game(array_input):
+    last_spoken = dict()
+    spoken_numbers = dict()
+    for idx in range(len(array_input)):
+        num = array_input[idx]
+        spoken_numbers[num] = [idx + 1]
+    last_number = array_input[-1]
+    counter = len(array_input) + 1
+    while True:
+        if counter == 30000001:
+            return last_number
+        if last_number not in last_spoken:
+            last_number = 0
+            if last_number in spoken_numbers:
+                spoken_numbers[last_number] += [counter]
+            else:
+                spoken_numbers[last_number] = [counter]
+            last_spoken[last_number] = counter
+        else:
+            if last_number in spoken_numbers and len(spoken_numbers[last_number]) > 1:
+                a, b = spoken_numbers[last_number][-2:]
+                # a, b = turns
+                last_number = b - a
+                if last_number in spoken_numbers:
+                    spoken_numbers[last_number] += [counter]
+                else:
+                    spoken_numbers[last_number] = [counter]
+                last_spoken[last_number] = counter
+            else:
+                last_number = 0
+                spoken_numbers[last_number] += [counter]
+                last_spoken[last_number] = counter
+        counter += 1
 
 
 if __name__ == '__main__':
-    # puzzle_input = load('input-14')
-    value = 7.304
-    text, text_fraction = str(value).split('.')
-    size = len(text)
-    size_fraction = len(text_fraction)
-    res = [str(int(text[digit]) * 10 ** (size - digit - 1)) for digit in range(size) if text[digit] != '0']
-    # res_fraction = [str(f'{int(text_fraction[digit]) * 10 ** (size_fraction - digit - 1)}') for digit in range(size_fraction) if text_fraction[digit] != '0']
-    res_fraction = [f'{int(text_fraction[digit])}/{10 ** (digit + 1)}' for digit in range(size_fraction) if text_fraction[digit] != '0']
-    print(' + '.join(res + res_fraction))
+    puzzle_input = load('input-15')
+    res = game(puzzle_input)
+    print(res)
+
