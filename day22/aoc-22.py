48e5020d2e4e63c87de7d4c5fbccc75d07b815af
Paweł Olkiewicz <olkpaw@gmail.com>
2021-11-09 18:05:17 +0100

    Day 22 Part2 WIP

--- day22/aoc-22.py
+++ day22/aoc-22.py
@@ -1,3 +1,5 @@
+previous_decks = []
+
 def load(name):
     with open(name, "r") as file:
         content = file.readlines()
@@ -30,6 +32,62 @@ def play_game(player_a, player_b):
     return calculate_score(player_a, player_b)
 
 
+def play_sub_game(player_a, player_b):
+    if max(player_a) > max(player_b):
+        return player_a, []
+
+    else:
+        return [], player_b
+
+
+def play_recursive_round(player_a, player_b):
+    a_top = player_a[0]
+    b_top = player_b[0]
+
+    if len(player_a) - 1 >= a_top and len(player_b) - 1 >= b_top:
+        #  play recursive sub-game
+        print(player_a, player_b, 'sub-game')
+        a, b = play_sub_game(player_a[1:], player_b[1:])
+
+        if len(a) > len(b):
+            player_a = player_a[1:] + [a_top, b_top]
+            player_b = player_b[1:]
+        else:
+            player_a = player_a[1:]
+            player_b = player_b[1:] + [b_top, a_top]
+
+    else:
+        print(player_a, player_b, 'high-low')
+        if a_top > b_top:
+            player_a = player_a[1:] + [a_top, b_top]
+            player_b = player_b[1:]
+        else:
+            player_a = player_a[1:]
+            player_b = player_b[1:] + [b_top, a_top]
+
+    return player_a, player_b
+
+
+def play_recursive_game(player_a, player_b):
+    global previous_decks
+    previous_decks = [(player_a, player_b)]
+
+    while len(player_a) > 0 and len(player_b) > 0:
+        player_a, player_b = play_recursive_round(player_a, player_b)
+
+        # if player_a == previous_a and player_b == previous_b:
+        if (player_a, player_b) in previous_decks:
+            # calculate_score(player_a, [])
+            return player_a, []
+
+        # previous_a = player_a
+        # previous_b = player_b
+        previous_decks.append((player_a, player_b))
+
+    return player_a, player_b
+    # return calculate_score(player_a, player_b)
+
+
 def calculate_score(player_a, player_b):
     tab = player_a if len(player_a) > 0 else player_b
     score = 0
@@ -42,7 +100,13 @@ def calculate_score(player_a, player_b):
 
 if __name__ == '__main__':
     p1, p2 = load('input-22')
+    p1_part2, p2_part2 = p1[:], p2[:]
     result_part1 = play_game(p1, p2)
-    print(result_part1)
+    print(f'result_part1 = {result_part1}')
+    p1_part2, p2_part2 = play_recursive_game(p1_part2, p2_part2)
+    result_part2 = calculate_score(p1_part2, p2_part2)
+    print(f'result_part2 = {result_part2}')
+    print(f'result_part2 = {p2_part2}')
 
+# 30175 too low
 
