1b673c132c34ded9a06385f24015df4735d1ba07
Paweł Olkiewicz <olkpaw@gmail.com>
2021-12-26 18:05:41 +0100

    Day 24 WIP

--- 2021/day24/day24.py
+++ 2021/day24/day24.py
@@ -1,4 +1,5 @@
 import math
+from copy import copy
 
 
 def load(name: str) -> list[str]:
@@ -54,24 +55,74 @@ def alu(instructions: list[str], input_numbers) -> int:  # it's useless
     return variables['z']
 
 
+def reverse_alu(instructions: list[str]) -> list[int]:
+    possible_values = []
+    starting_variables = {'z': 0}
+
+    # looking for "mul a 0"
+    for instruction in instructions:
+        commands = instruction.split(' ')
+
+        if commands[0] == 'mul' and commands[2] == '0':
+            starting_variables[commands[1]] = 0
+
+    input_variable = instructions[0].split(' ')[1]
+    for possible_z in range(-27, 27):
+        for possible_value in range(1, 10):
+            variables = copy(starting_variables)
+            variables[input_variable] = possible_value
+            variables['z'] = possible_z
+
+            for instruction in instructions[1:]:
+                commands = instruction.split(' ')
+
+                if commands[0] == 'add':
+                    a = commands[1]
+                    a_value = 0 if a not in variables else variables[a]
+                    b = commands[2]
+                    b_value = int(b) if b[0] == '-' or b.isnumeric() else (0 if b not in variables else variables[b])
+                    variables[a] = a_value + b_value
+
+                elif commands[0] == 'mul':
+                    a = commands[1]
+                    a_value = 0 if a not in variables else variables[a]
+                    b = commands[2]
+                    b_value = int(b) if b[0] == '-' or b.isnumeric() else variables[b]
+                    variables[a] = a_value * b_value
+
+                elif commands[0] == 'div':
+                    a = commands[1]
+                    a_value = 0 if a not in variables else variables[a]
+                    b = commands[2]
+                    b_value = int(b) if b[0] == '-' or b.isnumeric() else variables[b]
+                    variables[a] = math.floor(a_value / b_value)
+
+                elif commands[0] == 'mod':
+                    a = commands[1]
+                    a_value = 0 if a not in variables else variables[a]
+                    b = commands[2]
+                    b_value = int(b) if b[0] == '-' or b.isnumeric() else variables[b]
+                    variables[a] = a_value % b_value
+
+                elif commands[0] == 'eql':
+                    a = commands[1]
+                    a_value = 0 if a not in variables else variables[a]
+                    b = commands[2]
+                    b_value = int(b) if b[0] == '-' or b.isnumeric() else variables[b]
+                    variables[a] = 1 if a_value == b_value else 0
+            else:
+                print(f'z={variables["z"]}')
+                if variables['z'] == 0:
+                    possible_values.append(possible_value)
+
+
+    return possible_values
+
+
 def part1(instructions: list[str]) -> int:
-    # return alu(instructions, '13579246899999')
-    input_numbers = 53579246899999
+    values = reverse_alu(instructions[-18:])
 
-    # for current_input in reversed(range(53579246899999, 93579246899999)):
-    for current_input in range(11111116671575, 93579246899999):
-        print(current_input)
-        current_input_str = str(current_input)
-
-        if current_input_str.count('0') > 0:
-            continue
-
-        is_valid = alu(instructions, current_input_str) == 0
-
-        if is_valid:
-            return current_input
-
-    return 0
+    return len(values)
 
 
 def part2(instructions: list[list]) -> int:
